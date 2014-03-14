12afba2d7f7c9c48b52357fc8c61618a37866c1a
Julie Kang <julie.kang@stanfordalumni.org>
2014-03-13 23:36:43 -0700

    Added specs.

---
+++ spec/right_meow_spec.rb
@@ -0,0 +1,42 @@
+require 'spec_helper'
+
+describe 'right_meow' do
+  describe "Time#meow" do
+    it "matches at the same instant" do
+      meow = Time.meow
+      now = Time.now
+      expect(now.to_i).to eq(meow.to_i)
+    end
+
+    it "does not match at a different instant" do
+      meow = Time.meow
+      sleep(2)
+      now = Time.now
+      expect(now.to_i).not_to eq(meow.to_i)
+    end
+  end
+
+  describe "DateTime#meow" do
+    it "matches at the same instant" do
+      meow = DateTime.meow
+      now = DateTime.now
+      expect(now.to_time.to_i).to eq(meow.to_time.to_i)
+    end
+
+    it "does not match at a different instant" do
+      meow = DateTime.meow
+      sleep(2)
+      now = DateTime.now
+      expect(now.to_time.to_i).not_to eq(meow.to_time.to_i)
+    end
+  end
+
+  describe "#not_meow" do
+    it "sleeps n seconds" do
+      pre = Time.now
+      not_meow(2)
+      post = Time.now
+      expect(post.to_i).to eq(pre.to_i + 2)
+    end
+  end
+end
